d928032ac74fb1163cf9889255a7a343034327a7
Philipp Henkel <philipphenkel@users.noreply.github.com>
2020-10-03 00:11:45 +0200

    Support transmission of screenshots

--- test.ts
+++ test.ts
@@ -1,9 +1,8 @@
 makerbit.connectESP(SerialPin.P0,SerialPin.P1)
 makerbit.connectWifi("network", "secret")
-makerbit.setMeeting("123-456-789-0")
-makerbit.setGroup(1)
-makerbit.connectMeetingRoom("123-456-789-0", 1)
+makerbit.connectMeetingRoom("123-456-789-0", "1")
 
 makerbit.onReceivedString(function (value: string) {})
 makerbit.onReceivedNumber(function (value: number) {})
+makerbit.onReceivedScreenshot(function (screenshot: Image) {})
 makerbit.onReceivedNameValue("counter", function (value: number) {})
--- zoom.ts
+++ zoom.ts
@@ -1,11 +1,15 @@
 namespace makerbit {
   interface EspState {
     subscriptions: Subscription[];
-    meetingId: string;
-    groupId: number;
+    meeting: string;
+    room: string;
   }
 
   let espState: EspState = undefined;
+
+  const SCREENSHOT_TOPIC = "_sc"
+  const STRING_TOPIC = "_st"
+  const NUMBER_TOPIC = "_nu"
 
   function normalize(value: string) {
     return value.replaceAll(" ", "");
@@ -30,7 +34,11 @@ namespace makerbit {
     for (let i = 0; i < espState.subscriptions.length; ++i) {
       const sub = espState.subscriptions[i];
       if (sub.name == nameValue[0]) {
-        sub.handler(nameValue[1]);
+        if (sub.name == SCREENSHOT_TOPIC) {
+          sub.handler(decodeImage(nameValue[0]));
+        } else {
+          sub.handler(nameValue[1]);
+        }
       }
     }
   }
@@ -56,9 +64,12 @@ namespace makerbit {
 
   class Subscription {
     name: string;
-    handler: (value: string | number) => void;
+    handler: (value: string | number | Image) => void;
 
-    constructor(name: string, handler: (value: string | number) => void) {
+    constructor(
+      name: string,
+      handler: (value: string | number | Image) => void
+    ) {
       this.name = normalize(name);
       serial.writeLine("sub " + this.name);
       this.handler = handler;
@@ -72,6 +83,7 @@ namespace makerbit {
   //% blockId="makerbit_zoom_on_receive_name_value"
   //% block="on zoom received %name"
   //% draggableParameters=reporter
+  //% handlerStatement=true
   //% weight=20
   export function onReceivedNameValue(
     name: string,
@@ -90,12 +102,13 @@ namespace makerbit {
   //% blockId="makerbit_zoom_on_receive_string"
   //% block="on zoom received"
   //% draggableParameters=reporter
+  //% handlerStatement=true
   //% weight=40
   export function onReceivedString(handler: (receivedString: string) => void) {
     if (!autoConnectToESP()) {
       return;
     }
-    espState.subscriptions.push(new Subscription("str", handler));
+    espState.subscriptions.push(new Subscription(STRING_TOPIC, handler));
   }
 
   /**
@@ -105,12 +118,29 @@ namespace makerbit {
   //% blockId="makerbit_zoom_on_receive_number"
   //% block="on zoom received"
   //% draggableParameters=reporter
-  //% weight=30
+  //% handlerStatement=true
+  //% weight=35
   export function onReceivedNumber(handler: (receivedNumber: number) => void) {
     if (!autoConnectToESP()) {
       return;
     }
-    espState.subscriptions.push(new Subscription("num", handler));
+    espState.subscriptions.push(new Subscription(NUMBER_TOPIC, handler));
+  }
+
+  /**
+   * Registers code to run when the micro:bit receives a screenshot.
+   */
+  //% subcategory="Zoom"
+  //% blockId="makerbit_zoom_on_receive_screenshot"
+  //% block="on zoom received"
+  //% draggableParameters=reporter
+  //% handlerStatement=true
+  //% weight=30
+  export function onReceivedScreenshot(handler: (receivedScreenshot: Image) => void) {
+    if (!autoConnectToESP()) {
+      return;
+    }
+    espState.subscriptions.push(new Subscription(SCREENSHOT_TOPIC, handler));
   }
 
   /**
@@ -156,8 +186,8 @@ namespace makerbit {
     if (!espState) {
       espState = {
         subscriptions: [],
-        meetingId: "" + randint(1111111111, 9999999999),
-        groupId: 1,
+        meeting: "" + randint(1111111111, 9999999999),
+        room: "1",
       };
 
       control.inBackground(readSerialMessages);
@@ -185,32 +215,88 @@ namespace makerbit {
   }
 
   /**
-   * Broadcasts a number to other micro:bits connected via the internet.
-   */
-  //% subcategory="Zoom"
-  //% blockId="makerbit_zoom_send_number"
-  //% block="zoom send number %value"
-  //% weight=70
-  export function sendNumber(value: number) {
-    if (!autoConnectToESP()) {
-      return;
-    }
-    publish("num", "" + value);
-  }
-
-  /**
    * Broadcasts a string to other micro:bits connected via the internet.
    */
   //% subcategory="Zoom"
   //% blockId="makerbit_zoom_send_string"
   //% block="zoom send string %value"
   //% value.shadowOptions.toString=true
-  //% weight=60
+  //% weight=70
   export function sendString(value: string) {
     if (!autoConnectToESP()) {
       return;
     }
-    publish("str", value);
+    publish(STRING_TOPIC, value);
+  }
+
+  /**
+   * Broadcasts a number to other micro:bits connected via the internet.
+   */
+  //% subcategory="Zoom"
+  //% blockId="makerbit_zoom_send_number"
+  //% block="zoom send number %value"
+  //% weight=65
+  export function sendNumber(value: number) {
+    if (!autoConnectToESP()) {
+      return;
+    }
+
+    publish(NUMBER_TOPIC, "" + Math.roundWithPrecision(value, 2));
+  }
+
+  function decodeImage (value: string): Image {
+    let bytes = base64decode(value)
+
+    let bits = 0
+    for (let i = 3; i >= 0; i--) {
+      bits = bits << 8;
+      bits = bits + bytes[i]
+    }
+
+    let img = images.createImage("");
+    for (let x = 4; x >= 0; x--) {
+        for (let y = 4; y >= 0; y--) {
+            img.setPixel(x, y, (bits & 0x01) == 1);
+            bits = bits >> 1
+        }
+    }
+    
+    return img;
+}
+
+function encodeImage(image: Image) : string{
+    let bits = 0;
+    for (let x = 0; x < 5; x++) {
+      for (let y = 0; y < 5; y++) {
+        bits = bits << 1;
+        if (image.pixel(x, y)) {
+          bits = bits + 1;
+        }
+      }
+    }
+
+    let bytes: number[] = [];
+    for (let index = 0; index < 4; index++) {
+      bytes.push(bits & 0xff);
+      bits = bits >> 8;
+    }
+
+    return base64encode(bytes);
+  }
+
+  /**
+   * Broadcasts a screenshot to other micro:bits connected via the internet.
+   */
+  //% subcategory="Zoom"
+  //% blockId="makerbit_zoom_send_screenshot"
+  //% block="zoom send screenshot"
+  //% weight=60
+  export function sendScreenshot() {
+    if (!autoConnectToESP()) {
+      return;
+    }
+
+    publish(SCREENSHOT_TOPIC, "" + encodeImage(led.screenshot()));
   }
 
   /**
@@ -234,65 +320,237 @@ namespace makerbit {
   }
 
   /**
-   * Sets the group id for internet communications. A micro:bit can only listen to one one group in a meeting at any time.
-   * @param id the group id between ``0`` and ``255``
-   */
-  //% subcategory="Zoom"
-  //% blockId="makerbit_zoom_set_group"
-  //% block="zoom set group %id"
-  //% id.defl=1 id.min=0 id.max=255
-  //% weight=75
-  //% blockHidden=1
-  export function setGroup(id: number) {
-    if (!autoConnectToESP()) {
-      return;
-    }
-    espState.groupId = id;
-    updateMqttRoot();
-  }
-
-  /**
-   * Sets the meeting id for internet communications. A micro:bit can only listen to one group in a meeting at any time.
-   * @param id the meeting id
-   */
-  //% subcategory="Zoom"
-  //% blockId="makerbit_zoom_set_meeting"
-  //% block="zoom set meeting %id"
-  //% id.defl=123-456-7890
-  //% weight=79
-  //% blockHidden=1
-  export function setMeeting(id: string) {
-    if (!autoConnectToESP()) {
-      return;
-    }
-    espState.meetingId = normalize(id);
-    updateMqttRoot();
-  }
-
-  /**
    * Sets the meeting and room for internet communications. A micro:bit can only listen to room at any time.
    * @param id the meeting id
    */
   //% subcategory="Zoom"
   //% blockId="makerbit_zoom_connect_meeting_room"
-  //% block="zoom connect to meeting %id and room %room"
+  //% block="zoom connect to meeting %meeting and room %room"
   //% meetingId.defl=123-456-7890
-  //% room.defl=1 room.min=0 room.max=255
+  //% room.defl=1
   //% weight=74
-  export function connectMeetingRoom(meetingId: string, room: number) {
+  export function connectMeetingRoom(meeting: string, room: string) {
     if (!autoConnectToESP()) {
       return;
     }
-    espState.groupId = room;
-    espState.meetingId = normalize(meetingId);
+    espState.room = room;
+    espState.meeting = normalize(meeting);
     updateMqttRoot();
   }
 
   function updateMqttRoot() {
     serial.writeString("mqtt-root ");
-    serial.writeString(espState.meetingId);
+    serial.writeString(espState.meeting);
     serial.writeString("/");
-    serial.writeNumber(espState.groupId);
+    serial.writeString(espState.room);
     serial.writeString("\n");
+  }
+
+  function base64encode(data: number[]): string {
+    const base64EncodeChars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    let out = "";
+    const len = data.length;
+    let i = 0;
+    while (i < len) {
+      const c1 = data[i++] & 0xff;
+      if (i == len) {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+        out += "==";
+        break;
+      }
+      const c2 = data[i++];
+      if (i == len) {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
+        out += base64EncodeChars.charAt((c2 & 0xf) << 2);
+        out += "=";
+        break;
+      }
+      const c3 = data[i++];
+      out += base64EncodeChars.charAt(c1 >> 2);
+      out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
+      out += base64EncodeChars.charAt(((c2 & 0xf) << 2) | ((c3 & 0xc0) >> 6));
+      out += base64EncodeChars.charAt(c3 & 0x3f);
+    }
+    return out;
+  }
+
+  function base64decode(str: string): number[] {
+    const base64DecodeChars = [
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      62,
+      -1,
+      -1,
+      -1,
+      63,
+      52,
+      53,
+      54,
+      55,
+      56,
+      57,
+      58,
+      59,
+      60,
+      61,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+      26,
+      27,
+      28,
+      29,
+      30,
+      31,
+      32,
+      33,
+      34,
+      35,
+      36,
+      37,
+      38,
+      39,
+      40,
+      41,
+      42,
+      43,
+      44,
+      45,
+      46,
+      47,
+      48,
+      49,
+      50,
+      51,
+      -1,
+      -1,
+      -1,
+      -1,
+      -1,
+    ];
+
+    let c1, c2, c3, c4;
+    let i, len;
+    let out = [];
+
+    len = str.length;
+    i = 0;
+
+    while (i < len) {
+      /* c1 */
+      do {
+        c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+      } while (i < len && c1 == -1);
+      if (c1 == -1) break;
+
+      /* c2 */
+      do {
+        c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+      } while (i < len && c2 == -1);
+      if (c2 == -1) break;
+
+      out.push((c1 << 2) | ((c2 & 0x30) >> 4));
+
+      /* c3 */
+      do {
+        c3 = str.charCodeAt(i++) & 0xff;
+        if (c3 == 61) return out;
+        c3 = base64DecodeChars[c3];
+      } while (i < len && c3 == -1);
+      if (c3 == -1) break;
+
+      out.push(((c2 & 0xf) << 4) | ((c3 & 0x3c) >> 2));
+
+      /* c4 */
+      do {
+        c4 = str.charCodeAt(i++) & 0xff;
+        if (c4 == 61) return out;
+        c4 = base64DecodeChars[c4];
+      } while (i < len && c4 == -1);
+      if (c4 == -1) break;
+      out.push(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
   }
 }
